8bcd402815069529514aa54482e400b5035a2e86
barbo <barbo@DESKTOP-5LSP9FP>
2017-11-12 15:59:31 -0600

    Ya se como modificar el sitemaster

--- Gami-ICT/Site.Master.designer.cs
+++ Gami-ICT/Site.Master.designer.cs
@@ -13,6 +13,33 @@ namespace Gami_ICT {
     public partial class SiteMaster {
         
         /// <summary>
+        /// Control NB_Registrar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlAnchor NB_Registrar;
+        
+        /// <summary>
+        /// Control NB_Iniciar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlAnchor NB_Iniciar;
+        
+        /// <summary>
+        /// Control NB_Cerrar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlAnchor NB_Cerrar;
+        
+        /// <summary>
         /// Control MainContent.
         /// </summary>
         /// <remarks>
